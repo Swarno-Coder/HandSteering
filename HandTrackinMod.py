7953be7203a582b61be4b989a072d40058a3ecd7
swarno-coder <swarnodipnag2004@gmail.com>
2023-09-02 10:39:01 +0530

    Initial commit

---
+++ HandTrackinMod.py
@@ -0,0 +1,70 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+class handDetector():
+    def __init__(self, mode = False, maxHands=2, modelCom=1,detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.modelComp = modelCom
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHand = mp.solutions.hands
+        self.hands = self.mpHand.Hands(self.mode, self.maxHands, self.modelComp,
+                                       self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        print("Handedness: ", self.results.multi_handedness)
+        tipLms = []
+        if self.results.multi_hand_landmarks:
+            if len(self.results.multi_hand_landmarks)>=2:
+                h, w, c = img.shape
+                for handLms in self.results.multi_hand_landmarks:
+                    if draw: self.mpDraw.draw_landmarks(img, handLms,
+                                                self.mpHand.HAND_CONNECTIONS,
+                                                connection_drawing_spec=
+                                                self.mpDraw.DrawingSpec((233,43,5),
+                                                                        thickness=3))
+                    lmList = []
+                    for id, lm in enumerate(handLms.landmark):
+                        cx, cy = int(lm.x * w), int(lm.y * h)
+                        lmList.append([id, cx, cy])
+                    #tipX = handLms.landmark[self.mpHand.HandLandmark.THUMB_TIP].x * w
+                    #tipY = handLms.landmark[self.mpHand.HandLandmark.THUMB_TIP].y * h
+                    tipLms.append(lmList)
+            else: print("Please provide both hands")
+        return img, tipLms
+
+def main():
+    cTime = pTime = 0
+
+    ##########################
+    wCam, hCam = 1080, 920
+    ##########################
+
+    cap = cv2.VideoCapture(1)
+    cap.set(3, wCam)
+    cap.set(4, hCam)
+    detector = handDetector()
+    while True:
+        success, img = cap.read()
+        img = cv2.flip(img, 1)
+        img, lm = detector.findHands(img)
+        print(lm)
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN,
+                    3, (255, 0, 0), 3)
+        cv2.imshow("Result", img)
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            break
+    cap.release()
+
+if __name__ == '__main__':
+    main()
---
+++ steering.py
@@ -0,0 +1,57 @@
+import cv2
+import HandTrackinMod as htm
+import numpy as np
+import time, math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+
+
+##########################
+wCam , hCam = 1080, 920
+##########################
+
+cTime = pTime = 0
+
+cap = cv2.VideoCapture(1)
+cap.set(3, wCam)
+cap.set(4, hCam)
+detector = htm.handDetector(maxHands=2,
+                            detectionCon=0.8,
+                            trackCon=0.8)
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    img, lmList = detector.findHands(img)
+    if len(lmList) != 0:
+        x1 , y1 = lmList[0][4][1], lmList[0][4][2]
+        x2 , y2 = lmList[1][4][1], lmList[1][4][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        cv2.circle(img, (x1, y1), 10, (255, 0, 219), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 10, (255, 0, 219), cv2.FILLED)
+        cv2.circle(img, (cx, cy), 10, (255, 0, 219), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 219), 4)
+
+        '''
+        length = math.hypot((x2-x1),(y2-y1))
+        volBar = np.interp(length, [48,280], [365, 115])
+        volPer = np.interp(length, [48,280], [0, 100])
+        print(int(length))
+        if length <= 48:
+            cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
+
+        cv2.rectangle(img, (55, 115), (105, 365), (234,76,97), 3)
+        cv2.rectangle(img, (55, int(volBar)), (105, 365), (234,76,97), cv2.FILLED)
+        cv2.putText(img, f'{int(volPer)}%', (40, 430), cv2.FONT_HERSHEY_COMPLEX,
+                    1.3, (255, 0, 0), 2)
+        '''
+
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (30, 70), cv2.FONT_HERSHEY_PLAIN,
+                3, (255, 0, 0), 3)
+    cv2.imshow('Vol Control', img)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
+cap.release()
