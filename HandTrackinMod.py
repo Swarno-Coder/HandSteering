745aa8fde87c51aaec10121256916d4359508ddf
swarno-coder <swarnodipnag2004@gmail.com>
2023-09-03 16:50:14 +0530

    Final Product Release

--- HandTrackinMod.py
+++ HandTrackinMod.py
@@ -1,6 +1,4 @@
-import cv2
 import mediapipe as mp
-import time
 from google.protobuf.json_format import MessageToDict
 
 class handDetector():
@@ -10,24 +8,17 @@ class handDetector():
         self.modelComp = modelCom
         self.detectionCon = detectionCon
         self.trackCon = trackCon
-
         self.mpHand = mp.solutions.hands
-        self.hands = self.mpHand.Hands(self.mode, self.maxHands, self.modelComp,
-                                       self.detectionCon, self.trackCon)
+        self.hands = self.mpHand.Hands(self.mode, self.maxHands, self.modelComp,self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
-    def findHands(self, img, draw=True):
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.results = self.hands.process(imgRGB)
+    def findHands(self, img, img_rgb, draw=True):
+        self.results = self.hands.process(img_rgb)
+        counter = []
         if self.results.multi_handedness:
             for idx, hand_handedness in enumerate(self.results.multi_handedness):
                 handedness_dict = MessageToDict(hand_handedness)
-                whichHand = (handedness_dict['classification'][0]['label'])
-                # print(whichHand)
-                if whichHand == "Left":
-                    RightHand = True
-                else:
-                    RightHand = False
+                counter.append(handedness_dict['classification'][0]['index'])
         tipLms = []
         if self.results.multi_hand_landmarks:
             if len(self.results.multi_hand_landmarks)>=2:
@@ -42,37 +33,8 @@ class handDetector():
                     for id, lm in enumerate(handLms.landmark):
                         cx, cy = int(lm.x * w), int(lm.y * h)
                         lmList.append([id, cx, cy])
-                    #tipX = handLms.landmark[self.mpHand.HandLandmark.THUMB_TIP].x * w
-                    #tipY = handLms.landmark[self.mpHand.HandLandmark.THUMB_TIP].y * h
                     tipLms.append(lmList)
             else: print("Please provide both hands")
+            if counter and tipLms:
+                if counter[0] == 1: tipLms[0], tipLms[1] = tipLms[1], tipLms[0]
         return img, tipLms
-
-def main():
-    cTime = pTime = 0
-
-    ##########################
-    wCam, hCam = 1080, 920
-    ##########################
-
-    cap = cv2.VideoCapture(1)
-    cap.set(3, wCam)
-    cap.set(4, hCam)
-    detector = handDetector()
-    while True:
-        success, img = cap.read()
-        img = cv2.flip(img, 1)
-        img, lm = detector.findHands(img)
-        print(lm)
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-        cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN,
-                    3, (255, 0, 0), 3)
-        cv2.imshow("Result", img)
-        if cv2.waitKey(5) & 0xFF == ord('q'):
-            break
-    cap.release()
-
-if __name__ == '__main__':
-    main()
--- steering.py
+++ steering.py
@@ -1,16 +1,16 @@
-import cv2
+import cv2, time
 import HandTrackinMod as htm
-import numpy as np
-import time, math
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-
+import win32api, win32con
 
 ##########################
 wCam , hCam = 1080, 920
+cTime = pTime = 0
 ##########################
 
-cTime = pTime = 0
+def pressKey(key):
+    win32api.keybd_event(key, 0, 0, 0)
+    time.sleep(.05)
+    win32api.keybd_event(key, 0, win32con.KEYEVENTF_KEYUP, 0)
 
 cap = cv2.VideoCapture(1)
 cap.set(3, wCam)
@@ -21,7 +21,8 @@ detector = htm.handDetector(maxHands=2,
 while True:
     success, img = cap.read()
     img = cv2.flip(img, 1)
-    img, lmList = detector.findHands(img)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img, lmList = detector.findHands(img, imgRGB, draw=False)
     if len(lmList) != 0:
         x1 , y1 = lmList[0][4][1], lmList[0][4][2]
         x2 , y2 = lmList[1][4][1], lmList[1][4][2]
@@ -30,30 +31,17 @@ while True:
         cv2.circle(img, (x2, y2), 10, (255, 0, 219), cv2.FILLED)
         cv2.circle(img, (cx, cy), 10, (255, 0, 219), cv2.FILLED)
         cv2.line(img, (x1, y1), (x2, y2), (255, 0, 219), 4)
-        if y1-y2 < -int(img.shape[0] * 0.05): print ("D")
-        elif y1-y2 > int(img.shape[0] * 0.05): print ("A")
+        if y1-y2 < -int(img.shape[0] * 0.05): pressKey(0x44)
+        elif y1-y2 > int(img.shape[0] * 0.05): pressKey(0x41)
         else: print ("Straight")
-        '''
-        length = math.hypot((x2-x1),(y2-y1))
-        volBar = np.interp(length, [48,280], [365, 115])
-        volPer = np.interp(length, [48,280], [0, 100])
-        print(int(length))
-        if length <= 48:
-            cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
-
-        cv2.rectangle(img, (55, 115), (105, 365), (234,76,97), 3)
-        cv2.rectangle(img, (55, int(volBar)), (105, 365), (234,76,97), cv2.FILLED)
-        cv2.putText(img, f'{int(volPer)}%', (40, 430), cv2.FONT_HERSHEY_COMPLEX,
-                    1.3, (255, 0, 0), 2)
-        '''
-
-
+        p1, p2, p3, p4 = lmList[0][8][2] < lmList[0][5][2],lmList[0][12][2] < lmList[0][9][2],lmList[0][16][2] < lmList[0][13][2],lmList[0][20][2] < lmList[0][17][2]
+        q1, q2, q3, q4 = lmList[1][8][2] < lmList[1][5][2],lmList[1][12][2] < lmList[1][9][2],lmList[1][16][2] < lmList[1][13][2],lmList[1][20][2] < lmList[1][17][2]
+        if (p1 and p2 and p3 and p4 and q1 and q2 and q3 and q4): pressKey(0x20)
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, f'FPS: {int(fps)}', (30, 70), cv2.FONT_HERSHEY_PLAIN,
-                3, (255, 0, 0), 3)
-    cv2.imshow('Vol Control', img)
+    cv2.putText(img, f'FPS: {int(fps)}', (30, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+    cv2.imshow('Steering using HandGesture', img)
     if cv2.waitKey(5) & 0xFF == ord('q'):
         break
 cap.release()
